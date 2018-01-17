286e7365ebd64966a82c1275d9439628d4f250ab
fsdrasfragoso <esdrasfragoso@guithub.com>
2018-01-16 23:52:44 -0300

    iniciando partes logica do sistema!

---
+++ TriagemEletronica/src/triagemeletronica/array/Comparable.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package triagemeletronica.array;
+
+import java.util.ArrayList;
+import triagemeletronica.paciente.Paciente;
+
+/**
+ *
+ * @author Esdras Fragoso
+ */
+
+
+
+public interface Comparable {
+    
+     public ArrayList<Paciente> insertionSort(ArrayList<Float> list, ArrayList<Paciente> p);
+}
